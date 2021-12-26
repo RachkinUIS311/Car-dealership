0d1e7c1222b6ff95e1a91d8217c7a700ebfccac0
RachkinUIS311 <92579315+RachkinUIS311@users.noreply.github.com>
2021-12-26 16:49:07 +0300

    Create Car.cs

---
+++ Core/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class Car
+    {
+        public Car(string vin, string model, string color, decimal price)
+        {
+            this.VIN = vin ?? throw new ArgumentNullException(nameof(vin));
+            this.Model = model ?? throw new ArgumentNullException(nameof(model));
+            this.Color = color ?? throw new ArgumentNullException(nameof(color));
+            this.Price = price;
+        }
+
+        public string VIN { get; protected set; }
+
+        public string Model { get; protected set; }
+
+        public string Color { get; protected set; }
+
+        public decimal Price { get; protected set; }
+
+        public override string ToString()
+        {
+            return $"{this.VIN}, {this.Model}, {this.Color}, {this.Price}".Trim();
+        }
+    }
+}
