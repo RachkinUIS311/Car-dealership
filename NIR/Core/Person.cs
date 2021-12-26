6feec8ea5d3abab6c5126f4c1febaae1a32912d8
RachkinUIS311 <92579315+RachkinUIS311@users.noreply.github.com>
2021-12-26 17:23:55 +0300

    классы и тесты

---
+++ NIR/Core/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class Person
+    {
+        public Person(string name, string surname, string patronyomic, string sex)
+        {
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+            this.Surname = surname ?? throw new ArgumentNullException(nameof(surname));
+            this.Patronyomic = patronyomic ?? throw new ArgumentNullException(nameof(patronyomic));
+            this.Sex = sex ?? throw new ArgumentNullException(nameof(sex));
+        }
+
+        public string Name { get; protected set; }
+
+        public string Surname { get; protected set; }
+
+        public string Patronyomic { get; protected set; }
+
+        public string Sex { get; protected set; }
+
+        public override string ToString()
+            {
+               return $"{this.Surname}, {this.Name},  {this.Patronyomic}, {this.Sex}".Trim();
+            }
+
+    }
+}
---
+++ NIR/Core/Sell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class Sell 
+    {
+        public Sell(Car car, Staff staff, Client client,int sellNumber, DateTime date)
+        {
+            this.car = car;
+            this.staff = staff;
+            this.client = client;
+            SellNumber = sellNumber;
+            Date = date;
+        }
+
+        private Car car;
+        private Staff staff;
+        private Client client;
+
+        public int SellNumber { get; protected set; }
+
+        public DateTime Date { get; protected set; }
+
+        public override string ToString()
+        {
+            return $"Продавец:{staff.Surname}, {staff.Name}, {staff.Patronyomic}, {staff.Sex}, {staff.Position}, {staff.Salary} " +
+                $"Покупатель:{client.Surname},{client.Name},{client.Patronyomic},{client.Sex},{client.Phone} " +
+                $"Машина:{car.VIN}, {car.Model}, {car.Color}, {car.Price} " +
+                $"Договор:{this.SellNumber}, {this.Date}".Trim();
+
+        }
+    }
+}
