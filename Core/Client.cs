8068d19ea0198ddcebc7b89feb5c70fd053d7e24
RachkinUIS311 <92579315+RachkinUIS311@users.noreply.github.com>
2021-12-26 16:49:09 +0300

    Create Client.cs

---
+++ Core/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class Client : Person
+    {
+        public Client(int idClient, string phone, string name, string surname, string patronyomic,string sex)
+        : base(name, surname, patronyomic, sex)
+        {
+            this.IdClient = idClient;
+            this.Phone = phone ?? throw new ArgumentNullException(nameof(phone));
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+            this.Surname = surname ?? throw new ArgumentNullException(nameof(surname));
+            this.Patronyomic = patronyomic ?? throw new ArgumentNullException(nameof(patronyomic));
+            this.Sex = sex ?? throw new ArgumentNullException(nameof(sex));
+        }
+
+        public int IdClient { get; protected set; }
+
+        public string Phone { get; protected set; }
+
+        
+
+        public override string ToString()
+        {
+            return $"{this.Surname},{this.Name},{this.Patronyomic},{this.Sex},{this.Phone}".Trim();
+        }
+    }
+}
