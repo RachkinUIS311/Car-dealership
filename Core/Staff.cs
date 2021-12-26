c3d019d0a63c275b465be77a79552b98f93b9e1f
RachkinUIS311 <92579315+RachkinUIS311@users.noreply.github.com>
2021-12-26 16:52:41 +0300

    Create Staff.cs
    staff

---
+++ Core/Staff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class Staff : Person
+    {
+        public Staff(int idStaff, decimal salary, string position, string name, string surname, string patronyomic, string sex)
+            :base (name, surname, patronyomic, sex)
+        {
+            this.IdStaff = idStaff;
+            this.Position = position ?? throw new ArgumentNullException(nameof(position));
+            this.Salary = salary;
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+            this.Surname = surname ?? throw new ArgumentNullException(nameof(surname));
+            this.Patronyomic = patronyomic ?? throw new ArgumentNullException(nameof(patronyomic));
+            this.Sex = sex ?? throw new ArgumentNullException(nameof(sex));
+        }
+
+        public int IdStaff { get; protected set; }
+
+        public decimal Salary { get; protected set; }
+
+        public string Position { get; protected set; }
+
+        public override string ToString()
+        {
+            return $"{this.Surname}, {this.Name}, {this.Patronyomic}, {this.Sex}, {this.Position}, {this.Salary}".Trim();
+        }
+    }
+}
