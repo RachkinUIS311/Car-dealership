d5423a4462271aed9cba794fd05e803067425f73
RachkinUIS311 <92579315+RachkinUIS311@users.noreply.github.com>
2022-01-18 22:16:00 +0300

    Update Staff.cs

--- Core/Staff.cs
+++ Core/Staff.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +6,10 @@ namespace Core
 {
     public class Staff : Person
     {
+        [Obsolete("For ORM only", true)]
+        protected Staff () { }
         public Staff(int idStaff, decimal salary, string position, string name, string surname, string patronyomic, string sex)
-            :base (name, surname, patronyomic, sex)
+            :base (idStaff, name, surname, patronyomic, sex)
         {
             this.IdStaff = idStaff;
             this.Position = position ?? throw new ArgumentNullException(nameof(position));
@@ -18,15 +20,18 @@ namespace Core
             this.Sex = sex ?? throw new ArgumentNullException(nameof(sex));
         }
 
-        public int IdStaff { get; protected set; }
+        public virtual int IdStaff { get; protected set; }
 
-        public decimal Salary { get; protected set; }
+        public virtual decimal Salary { get; protected set; }
 
-        public string Position { get; protected set; }
+        public virtual string Position { get; protected set; }
 
         public override string ToString()
         {
             return $"{this.Surname}, {this.Name}, {this.Patronyomic}, {this.Sex}, {this.Position}, {this.Salary}".Trim();
         }
+        public virtual ISet<Person> Person { get; protected set; } = new HashSet<Person>();
+        public virtual ISet<Sell> Sells { get; protected set; } = new HashSet<Sell>();
     }
 }
+
