7a04f13d144d76d8687f530cca14a12721c08e64
RachkinUIS311 <92579315+RachkinUIS311@users.noreply.github.com>
2022-01-18 22:15:27 +0300

    Update Person.cs

--- Core/Person.cs
+++ Core/Person.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,26 +6,31 @@ namespace Core
 {
     public class Person
     {
-        public Person(string name, string surname, string patronyomic, string sex)
+        [Obsolete("For ORM only", true)]
+        protected Person() { }
+        public Person(int id, string name, string surname, string patronyomic, string sex)
         {
+            this.Id = id;
             this.Name = name ?? throw new ArgumentNullException(nameof(name));
             this.Surname = surname ?? throw new ArgumentNullException(nameof(surname));
             this.Patronyomic = patronyomic ?? throw new ArgumentNullException(nameof(patronyomic));
             this.Sex = sex ?? throw new ArgumentNullException(nameof(sex));
         }
 
-        public string Name { get; protected set; }
+        public virtual int Id { get; protected set; }
 
-        public string Surname { get; protected set; }
+        public virtual string Name { get; protected set; }
 
-        public string Patronyomic { get; protected set; }
+        public virtual string Surname { get; protected set; }
 
-        public string Sex { get; protected set; }
+        public virtual string Patronyomic { get; protected set; }
+
+        public virtual string Sex { get; protected set; }
 
         public override string ToString()
             {
-               return $"{this.Surname}, {this.Name},  {this.Patronyomic}, {this.Sex}".Trim();
+               return $"{this.Id}, {this.Surname}, {this.Name}, {this.Patronyomic}, {this.Sex}".Trim();
             }
-
     }
 }
+
